40d149528656da7fdae2f4f6edc6c1640d65aebb
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-08-13 21:18:18 +0600

    understanding array and 2d array concepts

---
+++ Basics-of-Cshrap/Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿int size = 5;
+int[] marks = new int[size];
+
+marks[0] = 40;
+marks[1] = 50;
+marks[2] = 60;
+marks[3] = 70;
+marks[4] = 80;
+//Console.WriteLine(marks[2]);
+
+int[,] grid = new int[3, 2];
+grid[0, 0] = 1;
+grid[0, 1] = 2;
+grid[1, 0] = 3;
+grid[1, 1] = 4;
+grid[2, 0] = 5;
+grid[2, 1] = 6;
+Console.WriteLine(grid[1, 1]);
--- Basics-of-Cshrap/DataTypes/Program.cs
+++ Basics-of-Cshrap/DataTypes/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿int x = 5;
+bool y = true;
+string s = "hello samir sadaf";
+char c = 's';
+double myDoubleNum = 5.80;
+byte b = 255;
