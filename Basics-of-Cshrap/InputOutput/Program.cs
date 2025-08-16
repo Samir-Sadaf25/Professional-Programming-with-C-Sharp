1104c07b7cf2605276bb51ee709c0dd6635b006d
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-08-17 00:00:23 +0600

    understanding input output concept in cshrap

---
+++ Basics-of-Cshrap/InputOutput/Program.cs
@@ -0,0 +1,21 @@
+﻿//Console.WriteLine("Hello my name is samir"); // print and create a new line
+//Console.WriteLine("i am currently learning c#");
+
+//Console.Write("Dev"); //print side by side
+//Console.Write("Skill");
+
+//string line = Console.ReadLine(); //string input from the user
+//char c = (char)Console.Read(); // charracter input from the user
+//ConsoleKeyInfo key = Console.ReadKey(); // key input from the user
+
+
+//int x;
+//string line = Console.ReadLine(); 
+//x = int.Parse(line); // int input from the user
+//Console.WriteLine(x);
+
+var nums = new int[5];
+for (int i = 0; i < 5; i++)
+{
+    nums[i] = int.Parse(Console.ReadLine());
+}
