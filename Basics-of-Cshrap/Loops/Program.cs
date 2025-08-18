ea7d3c4ff31e17a6b8ef9cf989d59cb314fd39ce
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-08-18 12:51:06 +0600

    program to print Hello and your name and print the sum of two numbers

--- Basics-of-Cshrap/Loops/Program.cs
+++ Basics-of-Cshrap/Loops/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var x = 328492;
+
+for (var i = 0; i < 5; i++)
+{
+    Console.WriteLine(x);
+}
+
+while (true)
+{
+    var line = Console.ReadLine();
+
+    if (line == string.Empty)
+        break;
+}
+
+
+int t = 0;
+do
+{
+    Console.WriteLine(t);
+    t++;
+
+} while (t > 0 && t < 5);
+
+var t2 = 0;
+
+while (t2 > 0 && t2 < 5)
+{
+    Console.WriteLine(t2);
+    t2++;
+
+}
+
+
+int[] ages = [33, 44, 55, 77];
+var sum = 0;
+foreach (var age in ages)
+{
+    sum = sum + age;
+}
+Console.WriteLine(sum);
+
+
+
---
+++ Basics-of-Cshrap/basic problem solving/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello : Samir Sadaf");
+int num1 = Convert.ToInt32(Console.ReadLine());
+double num2 = Convert.ToDouble(Console.ReadLine());
+double sum = num1 + num2;
+Console.WriteLine(sum);
+Console.ReadKey();
