f2711bc38455762e38566dbc0b0ab9d1e621c420
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-04 00:01:10 +0600

    solved(codeforces): A. Petya and Strings

---
+++ Problem solving in Cshrap/A_Petya_and_Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿string a = Console.ReadLine().ToLower();
+string b = Console.ReadLine().ToLower();
+int result = string.Compare(a, b);
+if (result > 0)
+{
+    Console.WriteLine("1");
+}
+else if (result < 0)
+{
+    Console.WriteLine("-1");
+}
+else
+{
+    Console.WriteLine("0");
+}
+Console.ReadKey();
+
