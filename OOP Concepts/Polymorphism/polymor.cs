f386b26a6fc48d52150d1023693d133f6c3fc8d2
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-03 21:13:59 +0600

    change file name and folder structure

---
+++ OOP Concepts/Polymorphism/polymor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class polymorphism
+    {
+         public int add(int a, int b)
+        {
+              return a + b;
+        }
+        public double add(double a, double b)
+        {
+              return a + b;
+        }
+        public string add(string a, string b)
+        {
+              return a + b;
+        }
+    }
+}
