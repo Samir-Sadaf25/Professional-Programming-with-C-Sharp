bedb780ffcb389a36ec5147b10989992b0f67be2
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-09 16:42:35 +0600

    solved(codeforces):339A - Helpful Maths

---
+++ Problem solving in Cshrap/339A_Helpful_Maths/Program.cs
@@ -0,0 +1,19 @@
+﻿string s = Console.ReadLine();
+char[] arr = s.ToCharArray();
+for (int i = 0; i < arr.Length; i+=2)
+{
+    int min_idx = i;
+    for (int j = i+2; j < arr.Length; j+=2)
+    {
+        if (arr[j] < arr[min_idx])
+        {
+            min_idx = j;
+        }
+        
+    }
+    char temp = arr[i];
+    arr[i] = arr[min_idx];
+    arr[min_idx] = temp;
+}
+
+Console.WriteLine(new string(arr));
