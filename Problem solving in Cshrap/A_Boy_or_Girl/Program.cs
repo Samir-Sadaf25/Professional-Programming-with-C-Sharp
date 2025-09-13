d2bfa33f9e8edf89af844db0f86955a4a0be8db5
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-13 16:59:51 +0600

    solved(codeforces): A. Boy or Girl

---
+++ Problem solving in Cshrap/A_Boy_or_Girl/Program.cs
@@ -0,0 +1,9 @@
+﻿string s = Console.ReadLine();
+HashSet<char> userSet = new HashSet<char>();
+for (int i = 0; i < s.Length; i++)
+{
+    userSet.Add(s[i]);
+}
+if (userSet.Count % 2 == 0)
+    Console.WriteLine("CHAT WITH HER!");
+else Console.WriteLine("IGNORE HIM!");
