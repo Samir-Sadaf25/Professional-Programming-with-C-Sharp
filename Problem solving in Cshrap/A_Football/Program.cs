3f3daec41be102e3c20701621b660f4bba89d154
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-13 17:18:49 +0600

    solved(codeforces): A. Football

---
+++ Problem solving in Cshrap/A_Football/Program.cs
@@ -0,0 +1,22 @@
+﻿string s = Console.ReadLine();
+int zero = 0;
+int one = 0;
+for (int i = 0; i < s.Length; i++)
+{
+    if (s[i] == '1')
+    {
+        one++;
+        zero = 0;
+    }
+    else
+    {
+        zero++;
+        one = 0;
+    }
+    if(zero >= 7 || one >= 7)
+    {
+        Console.WriteLine("YES");
+        return;
+    }
+}
+Console.WriteLine("NO");
