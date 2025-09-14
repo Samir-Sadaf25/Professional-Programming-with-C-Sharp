d778e311dfbd17ed81df99150d5f5652ede542e1
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-14 11:43:05 +0600

    solved(codeforces): A. Soldier and Bananas

---
+++ Problem solving in Cshrap/A. Soldier and Bananas/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string[] line = Console.ReadLine().Split(' ');
+int k = int.Parse(line[0]);
+int n = int.Parse(line[1]);
+int w = int.Parse(line[2]);
+
+int sum  = 0;
+for (int i = 1; i <= w; i++)
+{
+    sum += i;
+}
+int borrow = (sum * k) - n;
+if (borrow > 0)
+    Console.WriteLine(borrow);
+else Console.WriteLine(0);
