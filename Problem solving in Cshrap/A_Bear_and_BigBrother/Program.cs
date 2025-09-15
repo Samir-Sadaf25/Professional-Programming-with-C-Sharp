e9aafaae25306b01a9d28c85f46b93edfac3f76c
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-15 16:54:21 +0600

    solved(codeforces): A. Bear and Big Brother

---
+++ Problem solving in Cshrap/A_Bear_and_BigBrother/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] line = Console.ReadLine().Split(' ');
+int age1 = int.Parse(line[0]);
+int age2 = int.Parse(line[1]);
+int count = 0;
+while (age1 <= age2)
+{
+    age1 *= 3;
+    age2 *= 2;
+    count++;
+}
+Console.WriteLine(count);
