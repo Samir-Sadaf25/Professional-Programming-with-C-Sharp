8f49256799732044db2680f1302bb9fee9a63d50
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-16 14:02:47 +0600

    solved(codeforces): A. Wrong Subtraction

---
+++ Problem solving in Cshrap/A_Wrong_Subtraction/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] line = Console.ReadLine().Split(' ');
+int n = int.Parse(line[0]);
+int k = int.Parse(line[1]);
+for (int i = 0; i < k; i++)
+{
+    if (n % 10 == 0)
+    {
+        n = n / 10;
+    }
+    else n = n - 1;
+}
+Console.WriteLine(n);
