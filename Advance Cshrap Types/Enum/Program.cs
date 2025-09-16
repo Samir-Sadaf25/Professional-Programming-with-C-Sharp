f28093766d3aa18ba48d9ea4cbfdf2516626d3d0
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-16 16:51:29 +0600

    understand when we should use use enum type

---
+++ Advance Cshrap Types/Enum/Program.cs
@@ -0,0 +1,14 @@
+﻿using Enum;
+
+var user = new UserService().getUser("samir sadaf");
+if(user.Status == UserStatus.Active)
+{
+
+}
+else if (user.Status == UserStatus.Inactive)
+{
+
+}
+Console.WriteLine(UserStatus.Active.ToString());
+int x = (int)UserStatus.Inactive;
+Console.WriteLine(x);    
---
+++ Advance Cshrap Types/Enum/UserStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    public enum UserStatus
+    {
+       Inactive = 0,
+       Active = 1,
+       Block = 2,
+    }
+}
