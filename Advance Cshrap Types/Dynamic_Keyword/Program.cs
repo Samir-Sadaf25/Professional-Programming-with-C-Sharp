7ffa529917bd73ef9a773054162fc954e89c6060
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-22 15:28:01 +0600

    understand Dynamic keyword features

---
+++ Advance Cshrap Types/Dynamic_Keyword/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Net.Http.Headers;
+
+dynamic x = "hello";
+
+string y = x.Replace("ll", "mm");
+
+Console.WriteLine(y);
+
+dynamic r = new DateTime(2002, 5, 5);
+DateTime r2 = r.AddDays(3);
+Console.WriteLine(r2.Day);
+
+r = "hello";
+string r3 = r.Replace("ll", "mm");
+Console.WriteLine(r3);
+
+
+dynamic m = GetSomeData();
+Console.WriteLine(m.Price);
+
+
+object GetSomeData()
+{
+    return new { Price = 2000, Weight = 300 };
+}
