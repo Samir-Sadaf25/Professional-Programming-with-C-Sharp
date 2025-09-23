3d87be6ad1b7ebd5d653904c8ff4ba4a9a69ee12
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-09-23 20:07:52 +0600

    add example to understand the purpose and use cases of Generics

---
+++ Generics/Generics/BubbleSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class BubbleSort<T> where T : INumber<T>
+    {
+        public T[] items { get; set; }
+
+        public BubbleSort(T[] arr)
+        {
+            items = arr;
+        }
+
+        public void Sort()
+        {
+            T temp;
+            for (int j = 0; j <= items.Length - 2; j++)
+            {
+                for (int i = 0; i <= items.Length - 2; i++)
+                {
+                    if (items[i] > items[i + 1])
+                    {
+                        temp = items[i + 1];
+                        items[i + 1] = items[i];
+                        items[i] = temp;
+                    }
+                }
+            }
+          
+        }
+
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,8 @@
+﻿using Generics;
+
+BubbleSort<Double> sorting = new BubbleSort<double>([1.24, 8.25, 5.4, 7.6, 3.5]);
+sorting.Sort(); 
+foreach (var item in sorting.items)
+{
+    Console.WriteLine(item);
+}
