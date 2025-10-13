472956a7c7472563927ca75338e898883be03d79
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-10-13 20:26:29 +0600

    learn differences between Ienumerable and Icollection

--- ImportantInterfaces/Ienumerable/Keyboard.cs
+++ ImportantInterfaces/Ienumerable/Keyboard.cs
@@ -7,20 +7,70 @@ using System.Threading.Tasks;
 
 namespace Enumerable_Enumerator
 {
-    public class Keyboard : IEnumerable<Key>
+    public class Keyboard : ICollection<Key>
     {
-        private Key[] _keys = [new Key { Name = "A", Code = 65 }, new Key { Name = "B", Code = 66 }];
-       
+        private Key[] _keys = new Key[200];
+        private int idx = 0;
+
+        public int Count => idx;
+
+        public bool IsReadOnly => false;
+
+        public void Add(Key item)
+        {
+            if (idx >= _keys.Length) throw new InvalidOperationException("Keyboard is full");
+            _keys[idx++] = item;
+        }
+
+        public void Clear()
+        {
+            // Optional: clear references for GC
+            Array.Clear(_keys, 0, idx);
+            idx = 0;
+        }
+
+        public bool Contains(Key item)
+        {
+            // Use reference or value equality depending on Key semantics
+            for (int i = 0; i < idx; i++)
+            {
+                if (_keys[i] == item)
+                    return true;
+            }
+            return false;
+        }
+
+        public void CopyTo(Key[] array, int arrayIndex)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            if (arrayIndex < 0) throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            if (arrayIndex + Count > array.Length) throw new ArgumentException("Destination array is too small");
+
+            Array.Copy(_keys, 0, array, arrayIndex, idx);
+        }
 
         public IEnumerator<Key> GetEnumerator()
         {
-            return new KeyEnumerator(_keys);
+            for (int i = 0; i < idx; i++)
+                yield return _keys[i];
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+        public bool Remove(Key item)
         {
-           
-            return GetEnumerator();
+            for (int i = 0; i < idx; i++)
+            {
+                if (EqualityComparer<Key>.Default.Equals(_keys[i], item))
+                {
+                    // shift left
+                    int moveCount = idx - i - 1;
+                    if (moveCount > 0) Array.Copy(_keys, i + 1, _keys, i, moveCount);
+                    _keys[--idx] = null; // clear last slot for GC safety
+                    return true;
+                }
+            }
+            return false;
         }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
--- ImportantInterfaces/Ienumerable/Program.cs
+++ ImportantInterfaces/Ienumerable/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Enumerable_Enumerator;
+using System.Linq;
 
 var keyboard = new Keyboard();
-
+keyboard.Add(new Key { Name = "A", Code = 65 });
+keyboard.Add(new Key { Name = "B", Code = 66 });
 //foreach (var key in keys)
 //{
 //    Console.WriteLine($"{key.Name} : {key.Code}");
@@ -9,8 +11,11 @@ var keyboard = new Keyboard();
 
 foreach (var key in keyboard)
 {
-    Console.WriteLine($"{key.Name} : {key.Code}");
-    Console.WriteLine($"{key.Name} : {key.Code}");
+    //Console.WriteLine($"{key.Name} : {key.Code}");
+    
 
 }
 
+var a = new Key { Name = "A", Code = 65 };
+//Console.WriteLine(keyboard.Contains( new Key { Name = "A",Code = 65} ));
+Console.WriteLine(keyboard.Count);
