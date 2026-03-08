f797b600c40065d558993c83e2666e9cab72ead0
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-03-08 20:30:51 +0600

    understand the practical use cases of Ienumerable Interface also learn yield return keyword

---
+++ yieldReturn/Program.cs
@@ -0,0 +1,35 @@
+﻿using YeildReturn;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProcessPeople();
+    }
+    public static void ProcessPeople()
+    {
+        var people = GetPeople(10000000);
+        foreach(var person in people)
+        {
+            if (person.Id < 100000)
+            {
+                Console.WriteLine($"Id : {person.Id} Name: {person.Name}");
+            }
+            else
+                break;
+        }
+
+    }
+    static IEnumerable<Person> GetPeople(int count)
+    {
+       
+        for (int i = 0; i < count; i++)
+        {
+            yield return new Person() { Id = i, Name = $"Name {i}" };
+        }
+        
+    }
+}
+
+
+
