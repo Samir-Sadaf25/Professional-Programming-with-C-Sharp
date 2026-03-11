552bc5fd63844673d74535150b3199c584145d2e
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-03-11 16:26:39 +0600

    learn Operator over loading

---
+++ OperatorOverLoading/Fraction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverLoading
+{
+    public struct Fraction
+    {
+        public int num { get; private set; }
+        public int den { get; private set; }
+        public Fraction(int numerator, int denuminator)
+        {
+            if (denuminator == 0)
+            {
+                throw new ArgumentException("Denuminator cannot be zero",nameof(denuminator));
+            }
+            den = denuminator;
+            num = numerator;
+        }
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return new Fraction(a.num * b.den + b.num * a.den,a.den * b.den);
+        }
+    }
+}
---
+++ OperatorOverLoading/Program.cs
@@ -0,0 +1,7 @@
+﻿using OperatorOverLoading;
+
+Fraction f1 = new Fraction(2,3);
+Fraction f2 = new Fraction(4,5);
+Fraction result = f1 +  f2;
+
+Console.WriteLine($"{result.num} / {result.den}");
