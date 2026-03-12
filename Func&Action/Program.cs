9f7e047a2f297ceb70222fb65558d2a9d7cae9c9
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-03-12 13:44:14 +0600

    learn modern delegates Func and Action

---
+++ Func&Action/Program.cs
@@ -0,0 +1,24 @@
+﻿using Func_Action;
+
+BubbleSort2<int> sort = new BubbleSort2<int>();
+var result = sort.Sort([5,2, 4, 1, 3, 7, 6],checkCondition);
+
+bool checkCondition(int x, int y)
+{
+    //return x > y;
+    return x < y;
+}
+//foreach(var item in result)
+//{
+//    Console.WriteLine(item);
+//}
+
+Func<int, int, int> add = (a, b) => a + b;
+Func<int, int, int> multiply = (a, b) => a * b;
+
+Console.WriteLine(add(3,4));
+Console.WriteLine(multiply(3,4));
+
+Action<string> print = message => Console.WriteLine(message);
+
+print("hello i am samir..learning func and action");
