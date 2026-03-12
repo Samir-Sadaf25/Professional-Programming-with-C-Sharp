2a517664deb6c1e4770f6edba21eeb51e5ba0e77
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-03-12 13:08:27 +0600

    Learn what is Delegates and when to use it

---
+++ Delegates/BubbleSort2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class BubbleSort2<T>
+    {
+        public delegate bool Check(T a, T b);
+        public T[] Sort(T[] arr,Check condition)
+        {
+            T temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (condition(arr[i], arr[i + 1]))
+                    {
+                        temp = arr[i + 1]; arr[i + 1] = arr[i]; arr[i] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿using Delegates;
+
+
+
+BubbleSort2<int> bubbleSort = new BubbleSort2<int>();
+var result = bubbleSort.Sort([4, 2, 5, 3, 1, 7, 6], checkCondition);
+
+//foreach (int i in result)
+//{
+//    Console.WriteLine(i);
+//}
+
+bool checkCondition(int a, int b)
+{
+    //return a > b; // acending order
+    return a < b; // decending order
+}
+// simple example
+static int Add(int x, int y)
+{
+    return x + y;
+}
+ static int Multiply(int x, int y)
+{
+    return x * y;
+}
+
+Operation op;
+//op = Add;
+op = Multiply;
+int res = op(3, 4);
+//Console.WriteLine(result);
+
+delegate int Operation(int a, int b);
