36861e0b1cfccd2c201affe6adb0306a461d1ef5
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-03-25 17:39:34 +0600

    Learn: what is Threading,when and how to use Threading

---
+++ Threading/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Thread t1 = new Thread(() => PrintOdd(100));
+Thread t2 = new Thread(() => PrintEven(100));
+t1.Start();
+t2.Start();
+
+
+static void PrintOdd(object? count)
+{
+    for (int i = 1; i < (int)count; i+=2)
+    {
+        Console.WriteLine(i);
+      Thread.Sleep(200); // first e even execute hobe..then 200 mili sec. por por Odd execute hobe
+    }
+}
+//ar sleep na thakle 2 ta parallaly cholbe
+static void PrintEven(object? count)
+{
+    for (int i = 0; i < (int)count; i+=2)
+    {
+        Console.WriteLine(i);
+        //Thread.Sleep(200);
+    }
+}
