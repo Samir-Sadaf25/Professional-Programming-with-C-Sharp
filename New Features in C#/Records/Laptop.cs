a078af225ef1a265a59623ad2d145b171eb5ca4c
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-04-07 17:55:59 +0600

    Learn difference between class and records

---
+++ New Features in C#/Records/Laptop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Records
+{
+    public record Laptop
+    {
+        public string Brand { get; set; }
+        public int price { get; set; }
+    }
+}
---
+++ New Features in C#/Records/Program.cs
@@ -0,0 +1,10 @@
+﻿using Records;
+
+Laptop laptop1 = new Laptop { Brand = "HP", price = 50000 };
+Laptop laptop2 = new Laptop { Brand = "HP", price = 50000 };
+
+if (laptop1 == laptop2)
+{
+    Console.WriteLine("same"); // if we use record then we get same..cause record compare value 
+}
+else Console.WriteLine("not same"); // if we use class then we get not same..cause both of their memory is different
