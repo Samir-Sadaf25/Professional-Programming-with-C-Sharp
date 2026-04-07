2ccc581b54080b441ca4d0e5d402ea71737cc1ed
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-04-07 16:42:49 +0600

    Learn(new feature): Inverse indexing in C#

---
+++ New Features in C#/InverseIndexing/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] fruits = ["apple", "banana", "cherry", "jackfruits"];
+
+Console.WriteLine(fruits[0]);
+Console.WriteLine(fruits[1]);
+Console.WriteLine(fruits[2]);
+Console.WriteLine(fruits[3]);
+Console.WriteLine(" ");
+Console.WriteLine(fruits[^1]);
+Console.WriteLine(fruits[^2]);
+Console.WriteLine(fruits[^3]);
+Console.WriteLine(fruits[^4]);
+
+for (int i = 1; i < 5; i++)
+{
+    Console.WriteLine(fruits[^i]);
+}
