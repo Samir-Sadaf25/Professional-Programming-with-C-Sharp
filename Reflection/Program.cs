9fc2d6d36ce8ec15831508cf8694071e43043b0f
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-06-07 16:07:52 +0600

    update program.cs file

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -1,52 +1,90 @@
-﻿using Reflection;
+﻿//using Reflection;
+//using System;
+//using System.Reflection;
+//using System.Text.Json;
+
+//Type personType = typeof(Person);
+////Console.WriteLine(personType.FullName);
+
+////Console.WriteLine("Properties");
+//foreach (PropertyInfo property in personType.GetProperties())
+//{
+//   // Console.WriteLine("- " + property.Name + " (" + property.PropertyType.Name + ")");
+//}
+////Console.WriteLine("Methods:");
+////foreach (MethodInfo method in personType.GetMethods())
+////{
+////    Console.WriteLine("- " + method.Name);
+////}
+
+//Type animalType = typeof(Animal);
+//object animalInstance = Activator.CreateInstance(animalType);
+
+//PropertyInfo speciesProperty = animalType.GetProperty("Species");
+//PropertyInfo legsProperty = animalType.GetProperty("Legs");
+
+//speciesProperty.SetValue(animalInstance, "Dog");
+//legsProperty.SetValue(animalInstance, 4);
+
+////foreach (PropertyInfo prop in animalType.GetProperties())
+////{
+////    Console.WriteLine(prop.Name);
+////}
+
+//MethodInfo methodInfo = animalType.GetMethod("Describe");
+//Console.WriteLine(methodInfo);
+
+//ConstructorInfo constructorInfo = animalType.GetConstructor(new Type[] {});
+////Console.WriteLine(constructorInfo);
+
+//PropertyInfo propertyInfo = animalType.GetProperty("Legs");
+//PropertyInfo propertyInfo1 = animalType.GetProperty("Species");
+
+
+
+
+//// create instance of a class
+//object o = constructorInfo.Invoke(new object[] {});
+//propertyInfo.SetValue(o, 4);
+//propertyInfo1.SetValue(o, "Dog");
+//object result = methodInfo.Invoke(o, null);
+//Console.WriteLine(result);
+
 using System;
 using System.Reflection;
-using System.Text.Json;
 
-Type personType = typeof(Person);
-//Console.WriteLine(personType.FullName);
-
-//Console.WriteLine("Properties");
-foreach (PropertyInfo property in personType.GetProperties())
+public class Person
 {
-   // Console.WriteLine("- " + property.Name + " (" + property.PropertyType.Name + ")");
+    public string Name { get; set; }
+    public int Age { get; set; }
 }
-//Console.WriteLine("Methods:");
-//foreach (MethodInfo method in personType.GetMethods())
-//{
-//    Console.WriteLine("- " + method.Name);
-//}
 
-Type animalType = typeof(Animal);
-object animalInstance = Activator.CreateInstance(animalType);
+public static class ReflectionSerializer
+{
+    public static string SerializeToJson(object obj)
+    {
+        Type type = obj.GetType();
+        PropertyInfo[] properties = type.GetProperties();
+        string json = "{ ";
 
-PropertyInfo speciesProperty = animalType.GetProperty("Species");
-PropertyInfo legsProperty = animalType.GetProperty("Legs");
+        foreach (var prop in properties)
+        {
+            string name = prop.Name;
+            string value = prop.GetValue(obj)?.ToString() ?? "null";
+            json += $"\"{name}\": \"{value}\", ";
+        }
 
-speciesProperty.SetValue(animalInstance, "Dog");
-legsProperty.SetValue(animalInstance, 4);
+        json = json.TrimEnd(',', ' ') + " }";
+        return json;
+    }
+}
 
-//foreach (PropertyInfo prop in animalType.GetProperties())
-//{
-//    Console.WriteLine(prop.Name);
-//}
-
-MethodInfo methodInfo = animalType.GetMethod("Describe");
-Console.WriteLine(methodInfo);
-
-ConstructorInfo constructorInfo = animalType.GetConstructor(new Type[] {});
-//Console.WriteLine(constructorInfo);
-
-PropertyInfo propertyInfo = animalType.GetProperty("Legs");
-PropertyInfo propertyInfo1 = animalType.GetProperty("Species");
-
-
-
-
-// create instance of a class
-object o = constructorInfo.Invoke(new object[] {});
-propertyInfo.SetValue(o, 4);
-propertyInfo1.SetValue(o, "Dog");
-object result = methodInfo.Invoke(o, null);
-Console.WriteLine(result);
-
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person { Name = "Alice", Age = 30 };
+        string json = ReflectionSerializer.SerializeToJson(person);
+        Console.WriteLine(json);
+    }
+}
